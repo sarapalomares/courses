608c06835efbe00a921bd0ab55adcc94d713ce29
Sara Palomares <sarapalomares@me.com>
2017-01-23 18:42:36 -0800

    Initial Commit

--- CD_Django/main/apps/vinmyMVC/views.py
+++ CD_Django/main/apps/vinmyMVC/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'vinmyMVC/index.html')
@@ -6,3 +6,14 @@ def index(request):
 def show(request):
     print (request.method)
     return render(request, 'vinmyMVC/show_users.html')
+
+def create(request):
+    print (request.method)
+    if request.method == 'POST':
+        print ('*'*50)
+        print (request.POST)
+        print ('*'*50)
+        request.session['name'] = request.POST ['first_name']
+        return redirect ('/')
+    else:
+        return redirect ('/')
--- CD_Django/survey_form_assignment/apps/surveyform/urls.py
+++ CD_Django/survey_form_assignment/apps/surveyform/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^users$', views.show),
-    url(r'^new_user$', views.create)
+    url(r'^surveys/process$', views.process),
+    url(r'^result$', views.result),
 ]
